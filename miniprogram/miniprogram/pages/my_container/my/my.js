655d91d62504c31ba40b8a694349c7abf3a14d93
todofirst <514206278@qq.com>
2021-01-30 01:48:38 +0800

    完成个人中心主框架

--- miniprogram/miniprogram/pages/my_container/my/my.js
+++ miniprogram/miniprogram/pages/my_container/my/my.js
@@ -5,7 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isLogin: true,
+    basePath: '../../../images/my_images/funs_icons/',
+    fun_icons: [
+      {
+        icon: 'icon_business_auth.png',
+        url: '/pages/my_container/my_pages_included/business_auth/index',
+        tip: '机构认证'
+      },
+      {
+        icon: 'icon_hr_academy.png',
+        url: '/pages/my_container/my_pages_included/hr_academy/index',
+        tip: '红人学院'
+      },
+      {
+        icon: 'icon__accept_good.png',
+        url: '/pages/my_container/my_pages_included/good_accept_addr/index',
+        tip: '收货地址'
+      },
+      {
+        icon: 'icon_news_announce.png',
+        url: '/pages/my_container/my_pages_included/news_announce/index',
+        tip: '新闻公告'
+      },
+      {
+        icon: 'icon_help.png ',
+        url: '/pages/my_container/my_pages_included/help_center/index',
+        tip: '帮助中心'
+      },
+      {
+        icon: 'icon_invite.png',
+        url: '/pages/my_container/my_pages_included/invite_gift/index',
+        tip: '邀请有礼'
+      },
+      {
+        icon: 'icon_contact.png',
+        url: '/pages/my_container/my_pages_included/contact_us/index',
+        tip: '联系客服'
+      },
+      {
+        icon: 'icon_about.png',
+        url: '/pages/my_container/my_pages_included/about_us/index',
+        tip: '关于我们'
+      }
+    ]
   },
 
   /**
